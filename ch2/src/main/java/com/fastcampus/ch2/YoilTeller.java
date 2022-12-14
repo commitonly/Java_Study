082ecbd203b58fba25a51019785f76c816c47a8b
taemin <myloopmy@gmail.com>
2022-12-14 23:33:44 +0900

    System프린트가 아닌 브라우저에서 출력 // 요청과 응답받을 객체를 선언함으로써 톰캣을 통해서 // 요청을 주고 응답을 받아서 브라우저에 뿌리는 것 까지 성공

--- ch2/src/main/java/com/fastcampus/ch2/YoilTeller.java
+++ ch2/src/main/java/com/fastcampus/ch2/YoilTeller.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Calendar;
 
 //년월일을 입력하면 요일을 알려주는 프로그램
@@ -11,17 +14,17 @@ import java.util.Calendar;
 public class YoilTeller {//http://localhost:8080/getYoil?year=2021&month=10&day=1
     // public static void main(String[] args) {
     @RequestMapping("/getYoil")
-    public void main(HttpServletRequest request){
+    public void main(HttpServletRequest request, HttpServletResponse response) throws IOException { // 톰캣에게 요청하는 객체와 받을 객체를 선언해주는 것
         //1. 입력
         String year = request.getParameter("year");
         String month = request.getParameter("month");
         String day = request.getParameter("day");
 
+
+        //2. 작업
         int yyyy = Integer.parseInt(year);
         int mm = Integer.parseInt(month);
         int dd = Integer.parseInt(day);
-
-        //2. 작업
 
         Calendar cal= Calendar.getInstance();
         cal.set(yyyy, mm-1, dd -1);
@@ -30,7 +33,10 @@ public class YoilTeller {//http://localhost:8080/getYoil?year=2021&month=10&day=
         char yoil= "일월화수목금토".charAt(dayOfweek);
 
         //3. 출력
-        System.out.println(year + "년" + month + "월" + day + "일은 " );
-        System.out.println(yoil+ "요일입니다.");
+        response.setContentType("text/html"); //어떤 형태로 받을 것인지 작성 텍스트형태로 받음
+        response.setCharacterEncoding("utf-8"); // 한글로 깨지지 않고 잘 나오기 위해 작성
+        PrintWriter out = response.getWriter();// response객체에서 브라우저로의 출력 스트링을 얻는다.
+        out.println(year + "년" + month + "월" + day + "일은 " );
+        out.println(yoil+ "요일입니다.");
     }
 }
