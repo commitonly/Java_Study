849a250621b7f4ed2489c37c2afe2c0e25cf297b
taemin <myloopmy@gmail.com>
2022-12-14 18:46:21 +0900

    First Commit

---
+++ ch2/src/main/java/com/fastcampus/ch2/Hello.java
@@ -0,0 +1,11 @@
+package com.fastcampus.ch2;
+
+//1. 원격 호출 가능한 프로그램으로 등록.
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World2");
+		System.out.println("Testing");
+	}
+
+}
