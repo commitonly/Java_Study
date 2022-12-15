e7f67779eb6a61e048cf16b9f192d8221bb4381b
taemin <myloopmy@gmail.com>
2022-12-15 21:22:17 +0900

    MVC 패턴 관심사 분리 테스트중

--- ch2/src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
+++ ch2/src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
@@ -1,6 +1,7 @@
 package com.fastcampus.ch2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,7 +18,17 @@ public class YoilTellerMVC {//http://localhost:8080/getYoil?year=2021&month=10&d
     @RequestMapping("/getYoilMVC") // 맵핑주소를 MVC로 바꿔준다 맵핑은 중복되면 충돌다니까 중복되지 않게 해야한다
 //    public void main(HttpServletRequest request, HttpServletResponse response) throws IOException { // 톰캣에게 요청하는 객체와 받을 객체를 선언해주는 것
 //    public void main(String year, String month, String day,  HttpServletResponse response) throws IOException { // MVC패턴 분리를 위해 각각 선언해서 request로 통으로 받지않고 개별로 받음
-    public void main(int year, int month, int day,  HttpServletResponse response) throws IOException { // 하단에 Integer.parse를 쓰지않고 바로 int로 받기 위한 코드
+//    public void main(int year, int month, int day,  HttpServletResponse response) throws IOException { // 하단에 Integer.parse를 쓰지않고 바로 int로 받기 위한 코드
+    // 하단에서 return "yoil"이 문자열이여서 void -> String으로 변경해줘야 에러가 생기지 않는다
+//    public String main(int year, int month, int day,  HttpServletResponse response) throws IOException { // 하단에 Integer.parse를 쓰지않고 바로 int로 받기 위한 코드
+    public String main(int year, int month, int day,  Model model) throws IOException { // 모델을 적어주고 기존에 사용중이던 response는 더이상 필요없으니 지워줬다
+
+        // 1. 유효성 검사
+        if (!isValid(year,month,day))
+            return "yoilError";
+
+
+
         //1. 입력
 //        String year = request.getParameter("year");
 //        String month = request.getParameter("month");
@@ -31,18 +42,44 @@ public class YoilTellerMVC {//http://localhost:8080/getYoil?year=2021&month=10&d
 //        int dd = Integer.parseInt(day);
 
 
-        Calendar cal= Calendar.getInstance();
 //        cal.set(yyyy, mm-1, dd -1); 기존 방식
-        cal.set(year, month-1, day -1); // 위에서 직접 선언해줬으니까 원래 이름 그대로 적어줘야한다
 
-        int dayOfweek = cal.get(Calendar.DAY_OF_WEEK); //1:일요일 2:월요일 ...
-        char yoil= "일월화수목금토".charAt(dayOfweek);
+        char yoil = getYoil(year, month, day);
+
+        return "yoil"; //WEB-INF/views/yoil.jsp 를 이용해서 작업처리된 결과를 출력해라는 의미이고 반환타입은 문자열이므로 상단에 public void를 public String으로 변경해야함
+
 
         //3. 출력
-        response.setContentType("text/html"); //어떤 형태로 받을 것인지 작성 텍스트형태로 받음
-        response.setCharacterEncoding("utf-8"); // 한글로 깨지지 않고 잘 나오기 위해 작성
-        PrintWriter out = response.getWriter();// response객체에서 브라우저로의 출력 스트링을 얻는다.
-        out.println(year + "년" + month + "월" + day + "일은 " );
-        out.println(yoil+ "요일입니다.");
+
+        // 기존방식은 주석처리 하고 별도로 분리하여 사용함
+
+//        response.setContentType("text/html"); //어떤 형태로 받을 것인지 작성 텍스트형태로 받음
+//        response.setCharacterEncoding("utf-8"); // 한글로 깨지지 않고 잘 나오기 위해 작성
+//        PrintWriter out = response.getWriter();// response객체에서 브라우저로의 출력 스트링을 얻는다.
+//        out.println("<html>");
+//        out.println("<head>");
+//        out.println("</head>");
+//        out.println("<body>");
+//        out.println(year + "년" + month + "월" + day + "일은 " );
+//        out.println(yoil+ "요일입니다.");
+//        out.println("</body>");
+//        out.println("</html>");
+    }
+
+
+
+    // 메서드가 Private으로 설정되는 이유는 이 클래스 안에서만 사용되게 하기위해서이다
+
+
+    private boolean isValid(int year, int month, int day) {
+        return true;
+    }
+
+    private char getYoil(int year, int month, int day) {
+        Calendar cal= Calendar.getInstance();
+        cal.set(year, month -1, day -1); // 위에서 직접 선언해줬으니까 원래 이름 그대로 적어줘야한다
+
+        int dayOfweek = cal.get(Calendar.DAY_OF_WEEK); //1:일요일 2:월요일 ...
+        return "일월화수목금토".charAt(dayOfweek);
     }
 }
---
+++ ch2/src/main/webapp/WEB-INF/views/yoil.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html; charset=utf-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+<%-- 이렇게 미리 들어갈 값을 생각해서 만들어 놓는것을 EL이라고 한다 EL(Expression Language)--%>
+<%-- 아래에 이렇게 출력될 자리를 만들어놓으면 모델 객체에 값이 아래에 들어가서 클라이언트에게 전달되는 것이다 --%>
+<p> ${year}년 ${month}월 ${day}일은 ${yoil}입니다.</p>
+
+</body>
+</html>
