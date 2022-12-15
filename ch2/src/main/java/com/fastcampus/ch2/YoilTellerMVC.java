348650c2550f21bad7882a96728496fb965482f8
taemin <myloopmy@gmail.com>
2022-12-15 21:40:32 +0900

    불필요한 주석 제거 코드 정리

--- ch2/src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
+++ ch2/src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
@@ -27,54 +27,21 @@ public class YoilTellerMVC {//http://localhost:8080/getYoil?year=2021&month=10&d
         if (!isValid(year,month,day))
             return "yoilError";
 
-
-
-        //1. 입력
-//        String year = request.getParameter("year");
-//        String month = request.getParameter("month");
-//        String day = request.getParameter("day");
-
-
-        //2. 작업
-        //원래는 변환해줘야 하지만 상단에서 바로 int로 받았음으로 주석처리해도 괜찮다
-//        int yyyy = Integer.parseInt(year);
-//        int mm = Integer.parseInt(month);
-//        int dd = Integer.parseInt(day);
-
-
-//        cal.set(yyyy, mm-1, dd -1); 기존 방식
-
+        // 2. 요일 계산
         char yoil = getYoil(year, month, day);
 
+        // 3. 계산한 결과를 model에 저장
+        model.addAttribute("year", year);
+        model.addAttribute("month", month);
+        model.addAttribute("day", day);
+        model.addAttribute("yoil", yoil);
+
         return "yoil"; //WEB-INF/views/yoil.jsp 를 이용해서 작업처리된 결과를 출력해라는 의미이고 반환타입은 문자열이므로 상단에 public void를 public String으로 변경해야함
-
-
-        //3. 출력
-
-        // 기존방식은 주석처리 하고 별도로 분리하여 사용함
-
-//        response.setContentType("text/html"); //어떤 형태로 받을 것인지 작성 텍스트형태로 받음
-//        response.setCharacterEncoding("utf-8"); // 한글로 깨지지 않고 잘 나오기 위해 작성
-//        PrintWriter out = response.getWriter();// response객체에서 브라우저로의 출력 스트링을 얻는다.
-//        out.println("<html>");
-//        out.println("<head>");
-//        out.println("</head>");
-//        out.println("<body>");
-//        out.println(year + "년" + month + "월" + day + "일은 " );
-//        out.println(yoil+ "요일입니다.");
-//        out.println("</body>");
-//        out.println("</html>");
     }
-
-
-
     // 메서드가 Private으로 설정되는 이유는 이 클래스 안에서만 사용되게 하기위해서이다
-
-
     private boolean isValid(int year, int month, int day) {
         return true;
     }
-
     private char getYoil(int year, int month, int day) {
         Calendar cal= Calendar.getInstance();
         cal.set(year, month -1, day -1); // 위에서 직접 선언해줬으니까 원래 이름 그대로 적어줘야한다
