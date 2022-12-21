0ec4361cdfd8782ddc4183f8d853b134f13aca43
taemin <myloopmy@gmail.com>
2022-12-21 20:51:51 +0900

    회원가입 폼을 구현하고 servlet-context.xml 파일을 통해 경로 편의성 수정

--- ch2/src/main/java/com/fastcampus/ch2/YoilTellerMVC5.java
+++ ch2/src/main/java/com/fastcampus/ch2/YoilTellerMVC5.java
@@ -1,60 +1,63 @@
 package com.fastcampus.ch2;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.io.IOException;
 import java.util.Calendar;
 
-//년월일을 입력하면 요일을 알려주는 프로그램
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 public class YoilTellerMVC5 {
     @ExceptionHandler(Exception.class)
-    public String catcher(Exception ex){
-        ex.printStackTrace();
+    public String catcher(Exception ex) {
+        System.out.println("ex="+ex);
+
         return "yoilError";
     }
-    @RequestMapping("/getYoilMVC4") // 맵핑주소를 MVC로 바꿔준다 맵핑은 중복되면 충돌다니까 중복되지 않게 해야한다
-    public ModelAndView main(MyDate date) throws IOException { // 모델을 적어주고 기존에 사용중이던 response는 더이상 필요없으니 지워줬다
 
-        ModelAndView mv = new ModelAndView();
-//         1. 유효성 검사
-        if (!isValid(date))
-            return mv;
+    @RequestMapping("/getYoilMVC5") // http://localhost/ch2/getYoilMVC5?year=2021&month=10&day=1
+//  public String main(@ModelAttribute("myDate") MyDate date, Model m) { // 아래와 동일
+    public String main(@ModelAttribute MyDate date, Model m) { // @ModelAttribute사용, 반환 타입은 String
+        System.out.println("myDate="+date);
 
-        // 2. 요일 계산
-        char yoil = getYoil(date);
+        // 1. 유효성 검사
+        if(!isValid(date))
+            return "yoilError";
 
-        // 3. 계산한 결과를 model에 저장
-        mv.addObject("myDate", date);
-        mv.addObject("yoil", yoil);
+        // 2. 처리
+//        char yoil = getYoil(date);
 
-        // 4. 결과를 보여줄 view를 지정
-        mv.setViewName("yoil");
-        return mv;
+        // 3. Model에 작업한 결과를 저장
+        // @ModelAttribute 덕분에 MyDate를 저장안해도 됨. View로 자동 전달됨.
+//      m.addAttribute("myDate", date);
+//      m.addAttribute("yoil", yoil);
 
-//        return "yoil"; //WEB-INF/views/yoil.jsp 를 이용해서 작업처리된 결과를 출력해라는 의미이고 반환타입은 문자열이므로 상단에 public void를 public String으로 변경해야함
+        // 4. 작업 결과를 보여줄 뷰의 이름을 반환
+        return "yoil";
+    }
+
+    private @ModelAttribute("yoil") char getYoil(MyDate date) {
+        return getYoil(date.getYear(), date.getMonth(), date.getDay());
+    }
+
+    private char getYoil(int year, int month, int day) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(year, month - 1, day);
+
+        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+        return " 일월화수목금토".charAt(dayOfWeek);
     }
 
     private boolean isValid(MyDate date) {
-        return isValid(date.getYear(),date.getMonth(),date.getDay());
+        return isValid(date.getYear(), date.getMonth(), date.getDay());
     }
 
-    private char getYoil(MyDate date) {
-        return getYoil(date.getYear(),date.getMonth(),date.getDay());
-    }
-
-    // 메서드가 Private으로 설정되는 이유는 이 클래스 안에서만 사용되게 하기위해서이다
     private boolean isValid(int year, int month, int day) {
-        return true;
-    }
-    private char getYoil(int year, int month, int day) {
-        Calendar cal= Calendar.getInstance();
-        cal.set(year, month -1, day -1); // 위에서 직접 선언해줬으니까 원래 이름 그대로 적어줘야한다
+        if(year==-1 || month==-1 || day==-1)
+            return false;
 
-        int dayOfweek = cal.get(Calendar.DAY_OF_WEEK); //1:일요일 2:월요일 ...
-        return "일월화수목금토".charAt(dayOfweek);
+        return (1<=month && month<=12) && (1<=day && day<=31); // 간단히 체크
     }
 }
--- ch2/src/main/webapp/WEB-INF/views/yoil.jsp
+++ ch2/src/main/webapp/WEB-INF/views/yoil.jsp
@@ -9,7 +9,7 @@
 <%-- 이렇게 미리 들어갈 값을 생각해서 만들어 놓는것을 EL이라고 한다 EL(Expression Language)--%>
 <%-- 아래에 이렇게 출력될 자리를 만들어놓으면 모델 객체에 값이 아래에 들어가서 클라이언트에게 전달되는 것이다 --%>
 <h1>year=<%=request.getParameter("year")%></h1>
-<p> ${year}년 ${month}월 ${day}일은 ${yoil}입니다.</p>
+<p> ${myDate.year}년 ${myDate.month}월 ${myDate.day}일은 ${yoil}입니다.</p>
 
 </body>
 </html>
